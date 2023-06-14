a7fc4a58f83e6e05419a1ba9c699daef731eff45
M1310GB <michael.moreno@fundacion-jala.org>
2023-06-14 17:33:12 -0500

    cambio de sprites y agregar nuevas naves

---
+++ game/components/spaceship.py
@@ -0,0 +1,56 @@
+import pygame
+from pygame.sprite import Sprite
+from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+class Spaceship(Sprite):
+    X_POS = ( SCREEN_WIDTH // 2 ) - 40
+    Y_POS = 500
+    def __init__(self):
+        self.image = SPACESHIP
+        self.image = pygame.transform.scale(self.image, (90,90))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.player = Spaceship
+
+
+    def update(self, user_input):
+        if user_input[pygame.K_LEFT]:
+            self.move_left()
+        elif user_input[pygame.K_RIGHT]:
+            self.move_right()
+        elif user_input[pygame.K_UP]:
+            self.move_up()
+        elif user_input[pygame.K_DOWN]:
+            self.move_down()
+
+    def move_left(self):
+        if self.rect.left > 0:
+            self.rect.x = self.rect.x - 10
+        while self.rect.left == 0:
+            self.rect.x = SCREEN_WIDTH - 60
+
+    def move_right(self):
+        if self.rect.right < SCREEN_WIDTH + 1:
+            self.rect.x = self.rect.x + 10
+        while self.rect.right == SCREEN_WIDTH + 10:
+            self.rect.x = 40
+
+    def move_diagonal_right(self):
+        if self.rect.right < SCREEN_WIDTH + 1:
+            self.rect.x = self.rect.x + 10
+            self.rect.y = self.rect.y - 10
+
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y = self.rect.y - 10
+
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 70:
+            self.rect.y = self.rect.y + 10
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))    
+    
--- game/utils/constants.py
+++ game/utils/constants.py
@@ -3,28 +3,31 @@ import os
 
 # Global Constants
 TITLE = "Spaceships Game"
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1100
-FPS = 30
+FPS = 60
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
+ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/player_plane.png"))
 
 SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
 
-BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
+BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Fondo_ciudad.jpg'))
 
 HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
 
 DEFAULT_TYPE = "default"
 SHIELD_TYPE = 'shield'
 
-SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
-SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png"))
+SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/player_plane.png"))
+SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/player_plane_shield.png"))
 BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
 
 BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
-ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
-
+ENEMY_2 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_2.png"))
+ENEMY_3 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_3.png"))
+ENEMY_4 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_4.png"))
+ENEMY_5 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_5.png"))
+#ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
 FONT_STYLE = 'freesansbold.ttf'
